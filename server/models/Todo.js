ba1e1fe87f3c820b51b7e51416133e0bc8579cb5
GustafJ <gustaf89@hotmail.se>
2021-10-06 10:45:48 +0200

    add Schema for Todo

---
+++ server/models/Todo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  todos: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
