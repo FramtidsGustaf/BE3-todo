02b9a23fed0258271e525e202f0422c2f29385c8
GustafJ <gustaf89@hotmail.se>
2021-10-06 12:26:39 +0200

    add comment

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,6 +9,7 @@ exports.getAllUsers = (req, res, next) => {
 }
 
 exports.createUser = (req, res, next) => {
+  //TODO encrypt password
   const { fullName, displayName, password, email } = req.body
 
   const user = new User({
