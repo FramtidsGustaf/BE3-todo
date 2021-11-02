2e775e132405a8b20639e6309d488f156e0b8c21
GustafJ <gustaf89@hotmail.se>
2021-11-02 13:58:34 +0100

    add test to fe

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import App from './App';
+import AddTodoPage from './pages/AddTodoPage';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<AddTodoPage />);
+  const linkElement = screen.getByText(/Lägg till todo/i);
   expect(linkElement).toBeInTheDocument();
 });
